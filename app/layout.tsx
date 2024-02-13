f436db11a7d8f48fb2df6b960fca3ed284eb82de
ryan-verc <148745422+ryan-verc@users.noreply.github.com>
2024-02-13 17:49:32 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,10 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 
 export const metadata = {
-  metadataBase: new URL('https://influencer-registration.vercel.app'),
-  title: 'Influencer Registration',
+  metadataBase: new URL('https://offcampus.vercel.app'),
+  title: 'Jobs',
   description:
-    'Portal for influencers to register to take part in collabs and promotions with Vercel',
+    'Vercel Job Portal',
 }
 
 const inter = Inter({
